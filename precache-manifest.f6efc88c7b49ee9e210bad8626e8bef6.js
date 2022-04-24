86ccbab97418c9122103e7ff7985387901c5a642
jchayanan <chayanan_jame@hotmail.com>
2022-04-24 23:57:36 +0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.f6efc88c7b49ee9e210bad8626e8bef6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c47dac7063225ff127f9de37b2b7fa9",
+    "url": "/certificate_verification/index.html"
+  },
+  {
+    "revision": "eb7f3c70abc2ee8fb6f6",
+    "url": "/certificate_verification/static/css/main.cd1f5c5b.chunk.css"
+  },
+  {
+    "revision": "6fd39ddef6604867184e",
+    "url": "/certificate_verification/static/js/2.f326abe3.chunk.js"
+  },
+  {
+    "revision": "eb7f3c70abc2ee8fb6f6",
+    "url": "/certificate_verification/static/js/main.86f0c39e.chunk.js"
+  },
+  {
+    "revision": "8c6d5f6b563c011c6fac",
+    "url": "/certificate_verification/static/js/runtime-main.84ff01a8.js"
+  }
+]);
